b83993f9a60a48d29c363a696118050172860546
Abdulmajeed Alrashidi <alrashabdulmajeed@gmail.com>
2025-03-06 15:39:34 +0300

    Fix style issues, should get a 'Good Style' now 🙏

--- today.py
+++ today.py
@@ -1,7 +1,13 @@
-'''
+''' """A module computing buddy pair names for the day"""
 This is the module docstring
 '''
 
 
 def my_name_is():
     return "Mjeed42"
+
+def my_buddy_is():
+    '''
+    This is the function docstring
+    '''
+    return "ssaunier"
